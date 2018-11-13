18f6e84647ef300dacde4b426e94e7513f6343e7
Kristin Brewer-Lowe and Noah Schooler <noah@>
2018-11-13 14:51:47 -0800

    Add logic to replace e, o, I and S

---
+++ lib/leet_speak.rb
@@ -0,0 +1,30 @@
+def leet_speak(sentence)
+  word_array = sentence.split(" ")
+  sentence = []
+  word_array.each do |word|
+    leet_word = []
+    letters_array = word.split("")
+    counter = 0
+    letters_array.each do |letter|
+      if letter === "e"
+        leet_word.push("3")
+      elsif letter === "o"
+        leet_word.push("0")
+      elsif letter === "I"
+        leet_word.push("1")
+      elsif letter === "s"
+        if counter === 0 && letters_array[0] === "s"
+          leet_word.push("s")
+          counter += 1
+        else
+          leet_word.push("z")
+          # binding.pry
+        end
+      else
+        leet_word.push(letter)
+      end
+    end
+    sentence.push(leet_word.join(""))
+  end
+  sentence.join(" ")
+end
--- spec/leet_speak_spec.rb
+++ spec/leet_speak_spec.rb
@@ -1,3 +1,29 @@
 require('rspec')
 require('pry')
 require('leet_speak.rb')
+
+describe('#leet_speak') do
+  it('returns a string as is when no Leetspeak rules apply') do
+    expect(leet_speak("happy")).to(eq("happy"))
+  end
+
+  it('replaces every "e" in a string with a "3"') do
+     expect(leet_speak("leet")).to(eq("l33t"))
+  end
+
+  it('replaces every "o" in a string with a "0"') do
+    expect(leet_speak("boo boo")).to(eq("b00 b00"))
+  end
+
+  it('replaces every "I" in a string with a "1"') do
+    expect(leet_speak("I like Ike")).to(eq("1 lik3 1k3"))
+  end
+
+  it('replaces every "s" in a string with a "z"') do
+    expect(leet_speak("roses")).to(eq("r0z3z"))
+  end
+
+  it('does not replace the first letter when it is an s') do
+    expect(leet_speak("sassafrass")).to(eq("sazzafrazz"))
+  end
+end
